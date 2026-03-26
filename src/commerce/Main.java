c8711769f6573dfe4d4650f65ff46827ad088494
sighingpotato <jot0202@naver.com>
2026-03-26 15:24:00 +0900

    update

---
+++ src/commerce/Main.java
@@ -0,0 +1,12 @@
+package commerce;
+
+public class Main {
+    public static void main(String[] args) {
+        // 카테고리 생성
+        Category electronics = new Category("전자제품");
+        Category cloth = new Category("의류");
+        Category food = new Category("식품");
+
+
+    }
+}
