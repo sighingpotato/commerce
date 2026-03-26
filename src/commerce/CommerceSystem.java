673cb5b98fe1ba4300c6295e2322986195489ae0
sighingpotato <jot0202@naver.com>
2026-03-26 16:29:40 +0900

    update

--- src/commerce/CommerceSystem.java
+++ src/commerce/CommerceSystem.java
@@ -1,9 +1,11 @@
 package commerce;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class CommerceSystem {
     private List<Category> categories;
+    private Scanner scanner = new Scanner(System.in)
 
     public CommerceSystem(List<Category> categories) {
         this.categories = categories;
@@ -16,7 +18,30 @@ public class CommerceSystem {
             for (int i = 0; i < categories.size(); i++) { // 컴퓨터는 숫자를 0부터 센다.
                 System.out.println((i + 1) + ". " + categories.get(i).getCategoryName());
             }
+            System.out.println("0. 종료     | 프로그램 종료");
+            System.out.print("입력 <-");
 
+            int select = scanner.nextInt();
+            scanner.nextInt();
+
+            if (select == 0) {
+                System.out.println("커머스 플랫폼을 종료합니다.");
+                break;
+              // 카테고리 이동
+            } else if (select > 0 && select <= categories.size()) {
+                Category selectedCategory = categories.get(select - 1); // 1 누르면 0번째 카테고리
+                showCategory(selectedCategory);
+            } else {
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+    }
+
+    private void showCategory(Category category) {
+        System.out.println("[" + category.getCategoryName() + "]");
+        List<Product> products = category.getProducts();
+
+        for (int i = 0; i < products.size(); i++) {
         }
     }
 
