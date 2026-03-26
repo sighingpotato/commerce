77fb7ad91c445849ff2b930ee041080b2df40caa
sighingpotato <jot0202@naver.com>
2026-03-26 15:46:52 +0900

    update

--- src/commerce/CommerceSystem.java
+++ src/commerce/CommerceSystem.java
@@ -3,10 +3,21 @@ package commerce;
 import java.util.List;
 
 public class CommerceSystem {
-    private List<Product> products;
+    private List<Category> categories;
 
-    public CommerceSystem(List<Product> products) {
-        this.products = products;
+    public CommerceSystem(List<Category> categories) {
+        this.categories = categories;
+    }
+    // 작업 시작
+    public void start() {
+        while (true) {
+            System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
+            // 목록 출력
+            for (int i = 0; i < categories.size(); i++) { // 컴퓨터는 숫자를 0부터 센다.
+                System.out.println((i + 1) + ". " + categories.get(i).getCategoryName());
+            }
+
+        }
     }
 
 }
--- src/commerce/Main.java
+++ src/commerce/Main.java
@@ -1,11 +1,32 @@
 package commerce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // 카테고리 생성
         Category electronics = new Category("전자제품");
-        Category cloth = new Category("의류");
+        Category cloths = new Category("의류");
         Category food = new Category("식품");
+
+        // 상품 배열에 올리기
+        electronics.addProduct(new Product("Galaxy S24", 1200000, "최신 스마트폰", 40));
+        electronics.addProduct(new Product("iPhone 15", 1350000, "Apple의 최신 스마트폰", 30));
+        electronics.addProduct(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 10));
+        electronics.addProduct(new Product("Airpods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 60));
+
+        cloths.addProduct(new Product("코트", 100000, "롱 코트", 60));
+        cloths.addProduct(new Product("바지", 80000, "슬렉스 바지", 75));
+
+        food.addProduct(new Product("치킨", 15000, "양념 통닭", 35));
+        food.addProduct(new Product("고기", 10000, "돼지고기 1인분", 40));
+
+        // 전체 리스트
+        List<Category> allCategories = new ArrayList<>();
+        allCategories.add(electronics);
+        allCategories.add(cloths);
+        allCategories.add(food);
 
 
     }
